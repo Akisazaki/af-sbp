606ab9085c8fbd2dc98da71b7379863bc9fd80da
akisazaki <akisazaki@gmail.com>
2019-09-19 12:18:51 +0900

    aa

--- motor/parameter.py
+++ motor/parameter.py
@@ -26,9 +26,9 @@ SER_BAUDRATE = 115200
 # SER_DEVICE = "/dev/tty.SLAB_USBtoUART"
 
 ## IF RASPBERRY_PI
-# SER_DRV_FAN = "/dev/ttyUSB0"
+SER_DRV_FAN = "/dev/ttyUSB0"
 ## ELSE
-SER_DRV_FAN = "/dev/ttyUSB1"
+# SER_DRV_FAN = "/dev/ttyUSB1"
 ## END_IF
 
 SER_TIMEOUT = 0.1
