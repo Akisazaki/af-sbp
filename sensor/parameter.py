942325764dc20f7f7afb2ca1e29acc813e23238c
akisazaki <akisazaki@gmail.com>
2019-10-02 14:24:59 +0900

    fff

---
+++ sensor/parameter.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+
+'''
+Project:
+	Smart Mobile Air Purifier (SMAP) Prototype
+
+Title:
+	Mobile Platform Parameters and Registers
+
+Description
+	-set parameters for serial communication
+	-set parameters for controlling motors
+	-set parameters for computing the robot kinematics
+	-set motors' mechanical properties
+
+* Copyrighted by Mingyo Seo
+* Created on October 13 2018
+'''
+
+## Message
+SONAR_T_FRAME_ID = "base_sonar_top"
+SONAR_B_FRAME_ID = "base_sonar_bottom"
+IMU_FRAME_ID = "link_imu"
+
+## Sonar
+## http://docs.ros.org/melodic/api/sensor_msgs/html/msg/Range.html
+SONAR_T_FIELD_OF_VIEW = 0.875 # 1.0472 # 60 degrees (full of arc)
+SONAR_B_FIELD_OF_VIEW = 1.047 # 1.22173 # 70 degrees (full of arc)
+SONAR_T_MIN_RANGE = 0.1
+SONAR_B_MIN_RANGE = 0.1
+SONAR_T_MAX_RANGE = 0.6 # 0.30
+SONAR_B_MAX_RANGE = 0.2 # 0.15
+
+SONAR_HORN_FIELD_OF_VIEW = [0.6733496388, 0.564514844, 0.5398300729, 0.5134166609, 0.5087361171, 0.4899573263, 0.48457557, 0.4545105212, 0.5212047835]
+SONAR_DEF_FIELD_OF_VIEW = [0.7610127542, 0.6284637982, 0.7610127542, 0.6733496388, 0.6554770136, 0.6524875685, 0.5959938472, 0.559899645, 0.6032380925]
+
+#SONAR_T_FIELD_OF_VIEWS = SONAR_HORN_FIELD_OF_VIEW
+SONAR_T_FIELD_OF_VIEWS = SONAR_DEF_FIELD_OF_VIEW
+SONAR_B_FIELD_OF_VIEWS = SONAR_DEF_FIELD_OF_VIEW
+
+IR_FIELD_OF_VIEW = 0.174533
+IR_MIN_RANGE = 0.2
+IR_MAX_RANGE = 0.8
+IR_FRAMES = ["link_ir_front"]
+
+## Serial Communication Parameter ##
+
+# Communication Configuration
+SER_BAUDRATE = 115200
+
+## IF RASPBERRY_PI
+SER_DEVICE = "/dev/ttyUSB1"
+## ELSE
+# SER_DEVICE = "/dev/ttyUSB1"
+## END_IF
+
+SER_TIMEOUT = 0.1
+
+# Communication Protocol
+MSG_START = 't'
+MSG_ENABLE = 'e' 
+MSG_SONAR = 's'
+MSG_DUST = 'd'
+MSG_IR = 'r'
+MSG_IMU = 'i'
+MSG_INA = 'a'
+MSG_END = 'q'
+
+MSG_WIFI_ENABLE = 'w'
+MSG_WIFI_START = '<'
+MSG_WIFI_END = '>'
+MSG_WIFI_MAXIMUM = 500
+
+MSG_RESOLUTION = 4
+MSG_DEV_NUMBER = 1
+MSG_LENGTH = 9  # 5 + MSG_DEV_NUMBER * MSG_RESOLUTION
+
+
+# tb0|4132q
+
+
+IMU_IGNORE_COUNT = 50
+
+
+# Covarience
+
+COV_AX = 3.676657530528100E-07
+COV_AY = 2.853356311666220E-07
+COV_AZ = 3.108230152761650E-07
+
+COV_ANGULAR = [
+	COV_AX, 0.0, 0.0,
+	0.0, COV_AY, 0.0,
+	0.0, 0.0, COV_AZ
+]
+
+COV_LX = 1.036906039894480E-04
+COV_LY = 1.013369945998860E-04
+COV_LZ = 2.235569510522100E-04
+
+COV_LINEAR = [
+	COV_LX, 0.0, 0.0,
+	0.0, COV_LY, 0.0,
+	0.0, 0.0, COV_LZ
+]
+
+COV_OX = 2.372183896856830E-06
+COV_OY = 1.355307237983090E-08
+COV_OZ = 4.528043672210960E-08
+COV_OW = 3.742991942054960E-08
+
+COV_ORIENTATION = [
+	COV_OX, 0.0, 0.0,
+	0.0, COV_OY, 0.0,
+	0.0, 0.0, COV_OZ
+]
---
+++ sensor/port_sensor.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String, Float32
+from sensor_msgs.msg import Range, Imu, BatteryState
+from geometry_msgs.msg import Quaternion, Vector3
+
+import time
+import threading
+
+import sys
+import tty
+import termios
+import serial
+import math
+import parameter
+
+import command
+import parameter
+
+arming_flag = False
+
+rs485_lock = threading.Lock()
+
+sonar_seqs = [0, 0]
+sonar_pubs = [None, None]
+sonar_frame_ids = [parameter.SONAR_B_FRAME_ID, parameter.SONAR_T_FRAME_ID]
+sonar_min_ranges = [parameter.SONAR_B_MIN_RANGE, parameter.SONAR_T_MIN_RANGE]
+sonar_max_ranges = [parameter.SONAR_B_MAX_RANGE, parameter.SONAR_T_MAX_RANGE]
+sonar_field_of_views = [parameter.SONAR_B_FIELD_OF_VIEWS, parameter.SONAR_T_FIELD_OF_VIEWS]
+
+ir_seqs = [0]
+ir_pubs = [None]
+
+dust_pub  = None
+imu_seq = 0
+imu_pub = None
+battery_seq = 0
+battery_pub = None
+
+
+def listener():
+    global sonar_pubs
+    global dust_pub
+    global imu_pub
+    global battery_pub
+    global ir_pubs
+    rospy.init_node('sensor_node', anonymous=True)
+    sonar_pubs[0] = rospy.Publisher('sonar_bottom', Range, queue_size=1)
+    sonar_pubs[1] = rospy.Publisher('sonar_top', Range, queue_size=1)
+    ir_pubs[0] = rospy.Publisher('ir_center', Range, queue_size=1)
+    dust_pub = rospy.Publisher('dust', Float32, queue_size=1)
+    imu_pub = rospy.Publisher('imu', Imu, queue_size=1)
+    battery_pub = rospy.Publisher('battery', BatteryState, queue_size=1)
+    # rospy.spin()
+
+def dispose():
+    global sonar_pubs
+    global dust_pub
+    global imu_pub
+    global battery_pub
+    global ir_pubs
+
+    for index in range(0, len(sonar_pubs)):
+        if None != sonar_pubs[index]:
+            sonar_pubs[index].unregister()
+            sonar_pubs[index] = None
+    for index in range(0, len(ir_pubs)):
+        if None != ir_pubs[index]:
+            ir_pubs[index].unregister()
+            ir_pubs[index] = None
+    if None != dust_pub:
+        dust_pub.unregister()
+        dust_pub = None
+    if None != imu_pub:
+        imu_pub.unregister()
+        imu_pub = None
+    if None != battery_pub:
+        battery_pub.unregister()
+        battery_pub = None
+
+
+class key(threading.Thread):
+
+    def __init__(self, time_step, sensor_controller):
+        threading.Thread.__init__(self)
+        self.threadID = 0
+        self.name = "emergency_key"
+        self.time_step = time_step
+        self.time_init = time.time()
+        self.vel_spatial = 0
+        self.vel_angular = 0
+        self.sensor_controller = sensor_controller
+
+    def run(self):
+        '''
+        run the subscriber thread
+        returns:
+                bool [terhimation without an error]
+        '''
+
+        global arming_flag
+
+        while arming_flag:
+            ch = command.getch()
+
+            if ch == 'q':
+                arming_flag = False
+                break
+
+            time.sleep(self.time_step)
+
+        printout = 'Thread ' + str(self.threadID) + ' disabled\r\n'
+        sys.stdout.write(printout)
+        self.sensor_controller.shutdown()
+        return True
+
+
+def on_imu(qx,qy,qz,qw, gx,gy,gz, ax,ay,az):
+    global imu_pub
+    global imu_seq
+
+    imu_seq += 1
+    if imu_seq < parameter.IMU_IGNORE_COUNT:
+        return
+
+    msg = Imu()
+    
+    msg.header.seq = imu_seq
+    msg.header.stamp = rospy.Time.now()
+    msg.header.frame_id = parameter.IMU_FRAME_ID
+
+    msg.orientation.x = qx
+    msg.orientation.y = qy
+    msg.orientation.z = qz
+    msg.orientation.w = qw
+
+    msg.angular_velocity.x = gx
+    msg.angular_velocity.y = gy
+    msg.angular_velocity.z = gz
+
+    msg.linear_acceleration.x = ax
+    msg.linear_acceleration.y = ay
+    msg.linear_acceleration.z = az
+
+    msg.angular_velocity_covariance = parameter.COV_ANGULAR
+    msg.linear_acceleration_covariance = parameter.COV_LINEAR
+    msg.orientation_covariance = parameter.COV_ORIENTATION
+
+    imu_pub.publish(msg)
+
+
+def on_ir(id, range):
+    global ir_seqs
+    global ir_pubs
+
+    if math.isinf(range) or math.isnan(range):
+        return
+    
+    msg = Range()
+    msg.header.stamp = rospy.Time.now()
+    msg.field_of_view = parameter.IR_FIELD_OF_VIEW
+    msg.range = range
+    ir_seqs[id] += 1
+    msg.header.seq = ir_seqs[id]
+    msg.header.frame_id = parameter.IR_FRAMES[id]
+    msg.min_range = parameter.IR_MIN_RANGE
+    msg.max_range = parameter.IR_MAX_RANGE
+    ir_pubs[id].publish(msg)
+
+
+def on_sonar(id, range):
+    global sonar_seqs
+    global sonar_pubs
+    
+    if math.isinf(range) or math.isnan(range):
+        return
+    
+    msg = Range()
+    msg.header.stamp = rospy.Time.now()
+    msg.range = range
+    fieldOfViewTable = sonar_field_of_views[id]
+    sonar_seqs[id] += 1
+    msg.header.seq = sonar_seqs[id]
+    msg.header.frame_id = sonar_frame_ids[id]
+    msg.min_range = sonar_min_ranges[id]
+    msg.max_range = sonar_max_ranges[id]
+
+    weight = range * 0.1
+    index = int(round(weight, 0))
+    if index >= len(fieldOfViewTable) - 1:
+        msg.field_of_view = fieldOfViewTable[len(fieldOfViewTable) - 1]
+    else:
+        weight = weight % 1.0
+        msg.field_of_view = fieldOfViewTable[index] * (1.0 - weight) + fieldOfViewTable[index + 1] * weight
+
+    sonar_pubs[id].publish(msg)
+
+
+def on_range(sensor_type, id, range):
+    if parameter.MSG_SONAR == sensor_type:
+        on_sonar(id, range)
+    elif parameter.MSG_IR == sensor_type:
+        on_ir(id, range)
+
+
+def on_dust(id, value):
+    global dust_pub
+    dust = Float32()
+    dust.data = value
+    dust_pub.publish(dust)
+
+
+def on_ina(voltage, current, power):
+    global battery_pub
+    global battery_seq
+    msg = BatteryState()
+    battery_seq += 1
+    msg.header.seq = battery_seq
+    msg.voltage = voltage * 0.001
+    msg.current = current  * 0.001
+    msg.percentage = max(min((voltage * 0.001 - 18.8) / 25.1, 1.0), 0.0)
+    # === Constant values === 
+    msg.design_capacity = 4
+    msg.power_supply_technology = msg.POWER_SUPPLY_TECHNOLOGY_LIPO
+    msg.present = True
+    msg.cell_voltage = [None, None, None, None, None, None]
+    battery_pub.publish(msg)
+
+
+if __name__ == "__main__":
+
+    queue = []
+
+    try:
+        listener()
+
+        sensor_controller = command.controller(imuCallback=on_imu, rangeCallback=on_range, dustCallback=on_dust, inaCallback=on_ina)
+        set_key = key(0.005, sensor_controller)
+        # queue.append(observer_thread)
+
+        try:
+            arming_flag = True
+            set_key.start()
+
+            sensor_controller.enable()
+            sys.stdout.write('Sensor controller enabled\r\n')
+            time.sleep(5.0e-1)
+
+            # observer_thread.start()
+
+            time_init = time.time()
+
+            time.sleep(1.0)
+
+            while arming_flag and not rospy.is_shutdown():
+                # time.sleep(1.0e-1)
+                sensor_controller.receive()
+                time.sleep(1.0e-5)
+            
+            for s in queue:
+                s.join
+
+            sys.stdout.write("Exiting main thread\r\n")
+
+            sensor_controller.disable()
+            sys.stdout.write("Sensor controller disabled\r\n")
+            time.sleep(1.0e-1)
+
+        except Exception as e1:
+            print ("Error: unable to start thread")
+            print e1
+        
+        print ("Disposing")
+        dispose()
+        print ("Dispose complete")
+        exit(0)
+
+    except Exception as e2:
+        print ("Error: unable to generate thread")
+        print e2
